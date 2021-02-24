30f2312bf79cb3390c4be9c46699c406768f318d
RomanKonopelko <romank761@gmail.com>
2021-02-25 00:56:07 +0200

    feat: add menu and canvas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
-function App() {
-  return <></>;
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Canvas from "./Components/Canvas/Canvas";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/play">Play</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/play">
+            <Play />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Play() {
+  return (
+    <div>
+      <h2>Play Breakout</h2>
+      <Canvas />
+    </div>
+  );
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
---
+++ src/Components/Canvas/Canvas.js
@@ -0,0 +1,7 @@
+import s from "./Canvas.module.css";
+
+const Canvas = () => {
+  return <canvas id={s.canvas}></canvas>;
+};
+
+export default Canvas;
