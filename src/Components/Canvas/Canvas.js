c350386b8fbf7585fd55a61f647d9f01c1a60141
RomanKonopelko <romank761@gmail.com>
2021-02-25 01:34:49 +0200

    feat: add basic ball animation

--- src/Components/Canvas/Canvas.js
+++ src/Components/Canvas/Canvas.js
@@ -1,7 +1,25 @@
+import { useEffect, useRef } from "react";
 import s from "./Canvas.module.css";
 
 const Canvas = () => {
-  return <canvas id={s.canvas}></canvas>;
+  const canvasRef = useRef(null);
+  console.log(canvasRef);
+  let x = 0;
+  useEffect(() => {
+    const render = () => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      ctx.arc(x, 75, 10, 0, Math.PI * 2, true); // Outer circle
+      x += 0.1;
+      ctx.stroke();
+      requestAnimationFrame(render);
+    };
+    render();
+  }, []);
+
+  return <canvas id={s.canvas} ref={canvasRef}></canvas>;
 };
 
 export default Canvas;
